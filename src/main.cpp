238f74ad62e14919807acdb5319e949a0f849587
DavidM-Fox <david.mac.fox@gmail.com>
2021-02-14 09:05:14 -0600

    Creating new branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
+//#include "../inc/csv-parser.h"
 #include "../inc/avapi.h"
-#include "../inc/csv-parser.h"
-#include <chrono>
-#include <sstream>
+//#include "../inc/csv-parser.h"
+#include "../inc/rapidcsv.h"
 #include <vector>
 
 int main()
 {
-    std::string api_key = avapi::readFirstLineFromFile("../../api.key");
-    avapi::Quote quote("GME", api_key);
-    auto xy = quote.getIntraday("15min");
-    auto time = quote.getTimeCol();
+    // std::string api_key = avapi::readFirstLineFromFile("../../api.key");
+    // avapi::Quote quote("TSLA", api_key);
+    // auto xy = quote.getIntraday("15min");
+    // auto time = quote.getTimeCol();
+    //
+    // for (auto &val : time) {
+    // std::cout << val << '\n';
+    // }
+    // std::cout << std::endl;
+    rapidcsv::Document doc("../../data/intraday_15min_TSLA.csv");
+    std::vector<float> col = doc.GetColumn<float>("open");
 
-    for (auto &t : time) {
-
-        std::cout << t << '\n';
+    for (auto &i : col) {
+        std::cout << i << '\n';
     }
     std::cout << std::endl;
 }
