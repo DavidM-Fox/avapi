a30713a1b2b48b8a47899fde25e013352d626106
DavidM-Fox <david.mac.fox@gmail.com>
2021-02-15 09:35:45 -0600

    Edit

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 #include "../inc/avapi.h"
-#include "../inc/rapidcsv.h"
 
 int main()
 {
     std::string api_key = avapi::readFirstLineFromFile("../../api.key");
-    avapi::Quote quote("TSLA", api_key);
-    avapi::time_series series = quote.getIntradaySeries("15min");
+    avapi::Quote quote("AAPL", api_key);
+    avapi::time_series series = quote.getTimeSeries(avapi::WEEKLY, 10);
     avapi::printSeries(series);
 }
