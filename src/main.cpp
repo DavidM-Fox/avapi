ccf85b6fc737c9c13075863e95e62ffbe627129a
DavidM-Fox <david.mac.fox@gmail.com>
2021-03-02 10:48:23 -0600

    Fix main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 int main()
 {
     std::string api_key = avapi::readFirstLineFromFile("../../api.key");
-
     std::string stock = "GME";
 
     avapi::Stock gme(stock, api_key);
 
-    auto quote1 =
-        gme.getTimeSeries(avapi::Stock::INTRADAY, avapi::Stock::_15MIN);
+    auto quote1 = gme.getTimeSeries(avapi::Stock::INTRADAY, "30min");
     auto quote2 = gme.getTimeSeries(avapi::Stock::DAILY, 10);
 
     avapi::print(quote1);
