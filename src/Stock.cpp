e8a3da55ad97916188274ff8477077acdda30eee
DavidM-Fox <david.mac.fox@gmail.com>
2021-03-10 14:48:33 -0600

    Make GlobalQuote a subclass of CompanyStock

--- src/Stock.cpp
+++ src/Stock.cpp
@@ -102,9 +102,38 @@ TimeSeries CompanyStock::getTimeSeries(const avapi::SeriesType &type,
     return series;
 }
 
+/// @brief GlobalQuote constructor
+/// @param timestamp A Unix timestamp
+/// @param data std::vector<float> ordered: [Open, High, Low, Close, Volume,
+/// Prev_Close, Change, Change%]
+CompanyStock::GlobalQuote::GlobalQuote(const std::string &symbol,
+                                       const std::time_t &timestamp,
+                                       const std::vector<float> &data)
+    : symbol(symbol), timestamp(timestamp), quote_data(data)
+{
+}
+
+/// @brief print formatted avapi::GlobalQuote data
+void CompanyStock::GlobalQuote::printData()
+{
+    std::cout << std::setw(12) << std::left << "Timestamp:";
+    std::cout << std::setw(15) << std::right << timestamp << '\n';
+    for (size_t i = 0; i < quote_data.size(); ++i) {
+        std::string header = headers[i] + ":";
+        std::cout << std::setw(12) << std::left << header;
+        std::cout << std::setw(15) << std::right << std::fixed
+                  << std::setprecision(2) << quote_data[i];
+        std::cout << '\n';
+    }
+}
+
+const std::vector<std::string> CompanyStock::GlobalQuote::headers{
+    "Open",   "High",       "Low",    "Close",
+    "Volume", "Prev_Close", "Change", "Change%"};
+
 /// @brief   Return the symbol's latest global quote
 /// @returns The symbol's global quote as an avapi::GlobalQuote object
-GlobalQuote CompanyStock::getGlobalQuote()
+CompanyStock::GlobalQuote CompanyStock::getGlobalQuote()
 {
     api_call.resetQuery();
 
