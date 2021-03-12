d23aa4e358b7a552610199bd33a86ca37dd82386
DavidM-Fox <david.mac.fox@gmail.com>
2021-03-12 10:11:36 -0600

    Add getters

--- include/avapi/crypto/HealthIndex.hpp
+++ include/avapi/crypto/HealthIndex.hpp
@@ -14,6 +14,13 @@ public:
     std::string symbol;
 
     std::time_t &Timestamp() { return timestamp; }
+    std::string &Name() { return data[1]; }
+    std::string &FcasRating() { return data[2]; }
+    std::string &FcasScore() { return data[3]; }
+    std::string &DeveloperScore() { return data[4]; }
+    std::string &MarketMaturityScore() { return data[5]; }
+    std::string &UtilityScore() { return data[6]; }
+    std::string &Timezone() { return data[7]; }
     void Update();
     void printData();
 
